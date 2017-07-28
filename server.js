26e3c9fa563b07ef2159a2b56c6568eba2f96571
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-07-27 21:45:58 -0400

    Install Socket.io in project

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var session = require('express-session');
 var routes = require('./src/routes/routes.js');
+var http = require('http');
+var io = require('socket.io')(http);
 
 var app = express();
 
@@ -9,6 +11,20 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 routes(app, process.env);
+
+// Socket.io Connection
+io.on('connection', function(socket) {
+	socket.emit('connection', []);
+
+	socket.on('stock', function(stockData) {
+		console.log("Socket stock data");
+	});
+
+	socket.on('disconnect', function() {
+		console.log("Socket Disconnected!");
+	});
+
+});
 
 app.set('port', (process.env.PORT || 8080));
 
